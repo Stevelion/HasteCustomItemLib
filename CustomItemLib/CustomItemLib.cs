ce271f0c9828f5dad97a8c3194ff3f35b05900d6
Stevelion <54511192+Stevelion@users.noreply.github.com>
2025-04-19 00:11:50 -0400

    Added support for effectEvents

--- CustomItemLib/CustomItemLib.cs
+++ CustomItemLib/CustomItemLib.cs
@@ -24,6 +24,8 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Localization;
 using Landfall.Haste;
+using System.Reflection;
+using System.Collections;
 
 namespace CustomItemLib
 {
@@ -35,6 +37,14 @@ namespace CustomItemLib
             {
                 orig(self);
                 LoadCustomItems();
+            };
+
+            On.Player.RegisterItem += (orig, self, item) =>
+            {
+                GameObject cloneGameObject = item.gameObject;
+                ItemInstance templateItemInstance = ItemDatabase.instance.items.FirstOrDefault(e => e.name == item.itemName);
+                CloneRuntimeListeners(templateItemInstance, cloneGameObject);
+                orig(self, item);
             };
         }
 
@@ -56,18 +66,65 @@ namespace CustomItemLib
             }
         }
 
-        public static UnityAction CreateAction<T>(
-            T specialItemEffectInstance,  // The object you want to pull the effect method from ie.    `new SlomoItem { duration = 5f }`
-            Func<T, UnityAction> selector // A lambda expression to access that method dynamically ie. `obj => obj.Activate`
-        )
+        private static void CloneRuntimeListeners(ItemInstance template, GameObject clone)
         {
-            return selector(specialItemEffectInstance);
+            ItemInstance cloneItemInstance = clone.GetComponent<ItemInstance>();
+            // Clone effectEvent
+            CloneUnityEventRuntimeListeners(template.effectEvent, cloneItemInstance.effectEvent, clone);
+            // Clone keyDownEvent
+            CloneUnityEventRuntimeListeners(template.keyDownEvent, cloneItemInstance.keyDownEvent, clone);
+            // Clone keyUpEvent
+            CloneUnityEventRuntimeListeners(template.keyUpEvent, cloneItemInstance.keyUpEvent, clone);
         }
 
-        public static UnityEvent CreateUnityEvent(List<UnityAction> listeners)
+        private static void CloneUnityEventRuntimeListeners(UnityEvent templateEvent, UnityEvent cloneEvent, GameObject cloneObject)
+        {
+            // InvokableCallList that holds the runtime calls
+            var callListObj = typeof(UnityEventBase).GetField("m_Calls", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(templateEvent);
+            // List<BaseInvokableCall>
+            IList? runtimeCallList = callListObj.GetType().GetField("m_RuntimeCalls", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(callListObj) as IList;
+            if (runtimeCallList == null || runtimeCallList.Count == 0) return;
+            // Clone each UnityAction
+            UnityAction templateAction;
+            Component cloneEffectComponent;
+            foreach (var call in runtimeCallList)
+            {
+                templateAction = (UnityAction)call.GetType().GetField("Delegate", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(call);
+                // Add the MonoBehavior script as a component to the new item
+                cloneEffectComponent = cloneObject.GetComponent(templateAction.Target.GetType());
+                // Create a new UnityAction and add it as a listener to the new event
+                UnityAction unityAction = (UnityAction)Delegate.CreateDelegate(typeof(UnityAction), cloneEffectComponent, templateAction.Method);
+                cloneEvent.AddListener(unityAction);
+            }
+            return;
+        }
+
+        public class ActionDescriptor
+        {
+            public Type ClassType { get; }
+            public string MethodName { get; }
+
+            public ActionDescriptor(Type classType, string methodName)
+            {
+                ClassType = classType;
+                MethodName = methodName;
+            }
+        }
+
+        private static UnityEvent CreateUnityEvent(GameObject itemObject, List<ActionDescriptor> descriptors)
         {
             var unityEvent = new UnityEvent();
-            foreach (var listener in listeners) unityEvent.AddListener(listener);
+            foreach (var descriptor in descriptors)
+            {
+                Component effectComponent = itemObject.AddComponent(descriptor.ClassType);
+                MethodInfo method = descriptor.ClassType.GetMethod(descriptor.MethodName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+                if (method == null)
+                {
+                    Debug.LogError($"[CustomItemLib] Error: Method {descriptor.MethodName} not found on type {descriptor.ClassType.Name}");
+                }
+                UnityAction unityAction = (UnityAction)Delegate.CreateDelegate(typeof(UnityAction), effectComponent, method);
+                unityEvent.AddListener(unityAction);
+            }
             return unityEvent;
         }
 
@@ -177,9 +234,9 @@ namespace CustomItemLib
             List<ItemTrigger>? triggerConditions = null,
             List<ItemEffect>? effects = null,
             PlayerStats? stats = null,
-            UnityEvent? effectEvent = null,
-            UnityEvent? keyDownEvent = null,
-            UnityEvent? keyUpEvent = null
+            List<ActionDescriptor>? effectEvent = null,
+            List<ActionDescriptor>? keyDownEvent = null,
+            List<ActionDescriptor>? keyUpEvent = null
         )
         {
             ItemInstance? itemInstance = GetItemInstanceByItemName(itemName);
@@ -202,9 +259,9 @@ namespace CustomItemLib
                 if (triggerConditions is not null) itemInstance.triggerConditions = triggerConditions;
                 if (effects is not null) itemInstance.effects = effects;
                 if (stats is not null) itemInstance.stats = stats;
-                if (effectEvent is not null) itemInstance.effectEvent = effectEvent;
-                if (keyDownEvent is not null) itemInstance.keyDownEvent = keyDownEvent;
-                if (keyUpEvent is not null) itemInstance.keyUpEvent = keyUpEvent;
+                if (effectEvent is not null) itemInstance.effectEvent = CreateUnityEvent(itemInstance.gameObject, effectEvent);
+                if (keyDownEvent is not null) itemInstance.keyDownEvent = CreateUnityEvent(itemInstance.gameObject, keyDownEvent);
+                if (keyUpEvent is not null) itemInstance.keyUpEvent = CreateUnityEvent(itemInstance.gameObject, keyUpEvent);
             }
             else
             {
@@ -227,9 +284,9 @@ namespace CustomItemLib
                 itemInstance.triggerConditions = triggerConditions ?? new List<ItemTrigger> { };
                 itemInstance.effects = effects ?? new List<ItemEffect> { };
                 itemInstance.stats = stats ?? CreatePlayerStats();
-                itemInstance.effectEvent = effectEvent ?? new UnityEvent();
-                itemInstance.keyDownEvent = keyDownEvent ?? new UnityEvent();
-                itemInstance.keyUpEvent = keyUpEvent ?? new UnityEvent();
+                itemInstance.effectEvent = effectEvent != null ? CreateUnityEvent(itemInstance.gameObject, effectEvent) : new UnityEvent();
+                itemInstance.keyDownEvent = keyDownEvent != null ? CreateUnityEvent(itemInstance.gameObject, keyDownEvent) : new UnityEvent();
+                itemInstance.keyUpEvent = keyUpEvent != null ? CreateUnityEvent(itemInstance.gameObject, keyUpEvent) : new UnityEvent();
             }
             if (autoUnlocked)
             {
